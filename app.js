a443c3529447b2cbe5a8dfe5217b11b8f3e93cd2
Alice Williams <aliceklwilliams@gmail.com>
2018-03-17 10:36:32 +0000

    Added search function. Will show the first 10 results.

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 let express = require("express");
 let app = express();
 
+let fetch = require("node-fetch");
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/styles"));
+
+app.get("/search", function(req,res){
+    let filmName = req.query.filmName;
+    
+    fetch("http://www.omdbapi.com/?apikey=5f9b92a2&s="+filmName)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.Search.slice(0,10));
+        res.render("search", {films:data.Search.slice(0,10)});
+    });
+});
+
 app.get("/", function(req, res){
-    res.send("Welcome to my Film App!");
+    res.render("home");
 });
 
 app.listen(3000, function(){
