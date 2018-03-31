f632cf796eb9ec7d7475bc79b125ff94b8060afe
Alice Williams <aliceklwilliams@gmail.com>
2018-03-31 12:01:00 +0100

    Remove unnecessary return statements

--- routes/middleware.js
+++ routes/middleware.js
@@ -3,7 +3,7 @@ let Review = require("../models/Review");
 var middleware = {
     isLoggedIn: (req, res, next) => {
         if(req.isAuthenticated()){
-            return next();
+            next();
         } else{
             req.flash("error", "You need to be logged in.");
             res.redirect("/login");
@@ -14,7 +14,7 @@ var middleware = {
         if(req.isAuthenticated()){
             Review.findById(req.params.reviewID, (err, foundReview) =>{
                 if(foundReview.author.equals(req.user._id)){
-                    return next();
+                    next();
                 } else{
                     req.flash("error", "You need to be the review author.");
                     res.redirect("/");
