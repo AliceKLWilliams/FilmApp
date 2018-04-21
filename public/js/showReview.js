575a7ddec9fe37ceb01429e29e8628f35c9955eb
unknown <aliceklwilliams@gmail.com>
2018-04-21 14:57:26 +0100

    Add additional review ratings to film SHOW page, initially hidden.

---
+++ public/js/showReview.js
@@ -0,0 +1,18 @@
+let reviewButtons = document.querySelectorAll(".review .review__more");
+
+reviewButtons.forEach(button => { 
+	button.addEventListener("click", () => {
+		var ele = button.parentElement.querySelector(".review__extra");
+		var height = ele.scrollHeight;
+
+		if (!ele.classList.contains("review__extra--open")) {
+			ele.style.height = height + "px";
+			ele.classList.add("review__extra--open");
+			ele.style.height = "auto";
+		} else {
+			ele.style.height = height;
+			ele.style.height = "0px";
+			ele.classList.remove("review__extra--open");
+		}
+	});
+});
