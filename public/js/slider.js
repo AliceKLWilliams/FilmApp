a6990dfc1b70097e4c4fb0cde19c189c97f2dd50
unknown <aliceklwilliams@gmail.com>
2018-08-09 17:56:11 +0100

    + Add moving underline to the various sections of the film SHOW page.

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,13 +1,24 @@
 const links = document.querySelectorAll(".slider__link");
 const sections = document.querySelectorAll(".slider__content");
+const underline = document.querySelector(".slider__underline");
+
+let initialLeft = links[0].getBoundingClientRect().left;
+underline.style.width = links[0].getBoundingClientRect().width + "px";
+
 
 links.forEach(link => {
 	link.addEventListener("click", () => {
 		const name = link.dataset.name;
-		const content = document.querySelector(`.slider__content[data-name=${name}]`);
+		const content = document.querySelector(`.slider__content[data-name=${name}]`); // Get content for the link
 		
 		content.classList.add("slider__content--show");
 
+		const linkBoundingBox = link.getBoundingClientRect();
+
+		underline.style.left = (linkBoundingBox.left - initialLeft) + "px";
+		underline.style.width = linkBoundingBox.width + "px";
+
+		// Clean up classes
 		sections.forEach(section => {
 			if(section != content){
 				section.classList.remove("slider__content--show");
@@ -15,4 +26,8 @@ links.forEach(link => {
 			}
 		});
 	});
+});
+
+window.addEventListener("resize", () => {
+	initialLeft = links[0].getBoundingClientRect().left;
 });
